b8d20e3b05241f721ba887d30fcc9765255bb11a
FelipeBaltazar <felipe.dasilvabaltazar@gmail.com>
2018-04-23 17:41:37 -0300

    CrossApp has been created

---
+++ AllPodcast/AllPodcast/ViewModels/BaseViewModel.cs
@@ -0,0 +1,41 @@
+﻿using AllPodcast.Helpers;
+
+namespace AllPodcast.ViewModels
+{
+    public class BaseViewModel : ObservableObject
+    {
+        #region Local Variables
+
+        private string title = string.Empty;
+        private bool isBusy = false;
+
+        #endregion
+
+        #region Acessible Variables
+        
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
+        
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+
+        public virtual void OnAppearing()
+        {
+        }
+
+        public virtual void OnDisappearing()
+        {
+        }
+        
+
+		public IDataStore<Dog> DataStore => DependencyService.Get<IDataStore<Dog>>();
+
+        #endregion
+    }
+}
